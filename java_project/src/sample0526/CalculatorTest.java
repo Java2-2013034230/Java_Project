4521168116b15f5c9ff27924897fb0a684942836
khs6710 <khs6710@knu.ac.kr>
2016-05-26 20:46:19 +0900

    sample0526

---
+++ java_project/src/sample0526/CalculatorTest.java
@@ -0,0 +1,12 @@
+package sample0526;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Calculator C = new Calculator();
+	}
+
+}
